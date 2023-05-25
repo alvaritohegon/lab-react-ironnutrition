3319d7c4f873f70d5a0a692f086954b6dc8229cb
alvaritu <alvarohegon@gmail.com>
2023-05-25 09:03:01 +0200

    lab done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import foods from './foods.json';
 import './App.css';
+import FoodBox from './components/FoodBox';
 
 function App() {
+  //utilizamos el hook useState para crear una variable de estado llamada foodList y una función llamada setFoodList. Y el valor inicial de foodList lo establecemos con los datos importados del archivo foods.json.
+
+  const [foodList, setFoodList] = useState(foods);
+
   return (
+    //código JSX que va a ser renderizado
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* mapeamos la variable foodList, para poder recorrer cada elemento del array y realizar una acción para cada elemento. Y para cada elemento food en el array foodList se renderizará un elemento <div> con todos sus elementos. */}
+      {foodList.map((food, index) => (
+        // la propiedad key se establece con este valor que es necesario al renderizar una lista de elementos en React
+        <div key={index}>
+          <FoodBox
+            key={index}
+            title={food.name}
+            image={food.image}
+            calories={food.calories}
+            servings={food.servings}
+          />
+        </div>
+      ))}
+
+      <FoodBox
+        food={{
+          name: 'Orange',
+          calories: 85,
+          image: 'https://i.imgur.com/abKGOcv.jpg',
+          servings: 1,
+        }}
+      />
     </div>
   );
 }
